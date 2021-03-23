4f88fec71bba95c3a7e1e9e5af15f9b571d7f55e
Arnaud <choumelarnaud@gmail.com>
2021-03-23 12:37:22 +0100

    form creation for  curencies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,44 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Make a payment</div>
 
                 <div class="card-body">
-                    {{ __('You are logged in!') }}
+                    <form action="#" method="POST" id="paymentForm>">
+                        @csrf
+                        <div class="row">
+                            <div class="col-auto">
+                                <label for="">How much you want to pay?</label>
+                                <input
+                                type="number"
+                                name="value"
+                                min="5"
+                                step="0.01"
+                                class="form-control"
+                                value="{{mt_rand(500, 100000) / 100}}"
+                                required
+                                >
+                            </div>
+                            <div class="col-auto">
+                                <label for=""> Currency</label>
+                                <select
+                                    name="currency"
+                                    id=""
+                                    class="custom-select"
+                                    required
+                                    >
+                                    @foreach ($currencies as $currency)
+                                        <option value="{{$currency->iso}}">
+                                            {{strtoupper($currency->iso)}}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="text-center mt-3">
+                            <button type="submit" id="payButton" class="btn btn-primary btn-lg"> Pay</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
